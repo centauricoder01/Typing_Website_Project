9c9dba8b3f643a461e9acbd6d78125b8a2645045
Rajendra <itsrajendrapatel@gmail.com>
2022-12-08 10:57:23 +0530

    Images Added

--- Repeated_Script/Navbar.js
+++ Repeated_Script/Navbar.js
@@ -4,7 +4,7 @@ export const navigationBar = (
 ) => {
   return `<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
             <div class="container-fluid" id="navItems">
-                <a class="navbar-brand" href="../index.html">Food Store</a>
+                <a class="navbar-brand" href="../index.html">Typer</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
